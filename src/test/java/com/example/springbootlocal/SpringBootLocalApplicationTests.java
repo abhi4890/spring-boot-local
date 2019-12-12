5913a1615e000b76e5700b887ef426ef452a8cd3
Abhishek Katiyar <abhishek.katiyar@navi.com>
2019-12-12 12:30:20 +0530

    init

---
+++ src/test/java/com/example/springbootlocal/SpringBootLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootlocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
